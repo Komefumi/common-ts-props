d17a4486a0a762f8362755b34a38051da1afe046
Komefumi <komefumi@protonmail.ch>
2021-12-24 22:01:39 +0530

    1. change format of payload lists in global.ts -> make it more generic and give better naming 2. bump version of package to 0.0.4

--- global.ts
+++ global.ts
@@ -1,11 +1,17 @@
+export interface ITwoItemPayload<T, K> extends Array<any> {
+  0: T;
+  1: K;
+}
+export type ListOfTwoItemPayloads<T, K> = ITwoItemPayload<T, K>[];
+
+export interface IDoubleStringPayload extends ITwoItemPayload<string, string> {}
+// export type DoubleStringList = IDoubleStringPayload[];
+export type DoubleStringList = ListOfTwoItemPayloads<string, string>;
+
 /*
  * Use case: making an item like ["link-name", "/link-path"]
  * Make an array of such arrays, and loop through them to render links
  * for them in a DRY manner.
  * */
-export interface ILinkPayload extends Array<string> {
-  0: string;
-  1: string;
-}
-
-export type LinkPayloadList = ILinkPayload[];
+export interface ILinkPayload extends IDoubleStringPayload {}
+export type LinkPayloadList = DoubleStringList;
